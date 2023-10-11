fccde562b84c6281fb73bc1a14cf8337b5b843e5
Stefan Stefanov <stefanov.sm@abv.bg>
2023-10-11 09:34:02 +0300

    Update date_bucket.sql

--- date_bucket.sql
+++ date_bucket.sql
@@ -1,14 +1,14 @@
-create or replace function date_bucket(arg_datepart text, arg_number integer, arg_date timestamptz, arg_origin timestamptz default 'epoch')
+create or replace function date_bucket(dpart text, dpart_count integer, ts timestamptz, origin timestamptz default 'epoch')
 returns timestamptz language plpgsql immutable as
 $body$
 declare 
     DPARTS_RX constant text[] := '{^days?$,^weeks?$,^months?$,^quarters?$,^years?$,^hours?$,^minutes?$,^seconds?$,^milliseconds?$}';
     DPARTS_LIST constant text :=   'day(s), week(s), month(s), quarter(s), year(s), hour(s), minute(s), second(s), millisecond(s)'; 
 begin 
-    if not arg_datepart ~* any (DPARTS_RX) then 
-        raise exception 'date_bucket: invalid date part "%"', arg_datepart 
+    if not dpart ~* any (DPARTS_RX) then 
+        raise exception 'date_bucket: invalid date part "%"', dpart 
         using hint = 'Valid date parts are '||DPARTS_LIST;
     end if;
-    return date_bin(('1 '||arg_datepart)::interval * arg_number, arg_date, arg_origin);
+    return date_bin(('1 '||dpart)::interval * dpart_count, ts, origin);
 end;
 $body$;
