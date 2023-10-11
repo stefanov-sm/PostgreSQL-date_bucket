4d34969d838426def9563e69ac1073fc4b888643
Stefan Stefanov <stefanov.sm@abv.bg>
2023-10-11 09:37:10 +0300

    Update date_bucket.sql

--- date_bucket.sql
+++ date_bucket.sql
@@ -3,11 +3,10 @@ returns timestamptz language plpgsql immutable as
 $body$
 declare 
     DPARTS_RX constant text[] := '{^days?$,^weeks?$,^months?$,^quarters?$,^years?$,^hours?$,^minutes?$,^seconds?$,^milliseconds?$}';
-    DPARTS_LIST constant text :=   'day(s), week(s), month(s), quarter(s), year(s), hour(s), minute(s), second(s), millisecond(s)'; 
+    DPARTS_LIST constant text := 'Valid date parts are day(s), week(s), month(s), quarter(s), year(s), hour(s), minute(s), second(s), millisecond(s)'; 
 begin 
     if not dpart ~* any (DPARTS_RX) then 
-        raise exception 'date_bucket: invalid date part "%"', dpart 
-        using hint = 'Valid date parts are '||DPARTS_LIST;
+        raise exception 'date_bucket: invalid date part "%"', dpart using hint = DPARTS_LIST;
     end if;
     return date_bin(('1 '||dpart)::interval * dpart_count, ts, origin);
 end;
